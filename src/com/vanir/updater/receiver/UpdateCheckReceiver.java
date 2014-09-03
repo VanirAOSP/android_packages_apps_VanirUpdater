7e6853f6dceeb80129e4aaea8fe1d79656f320cf
PrimeDirective <activethrasher00@gmail.com>
2014-09-02 22:55:43 -0400

    VanirUpdater: remind the user if they forgot to install gapps
    because some people need a bit of a smack.
    
    Change-Id: I18605c770cbe770f8134719b3134c575effc9100

--- src/com/vanir/updater/receiver/UpdateCheckReceiver.java
+++ src/com/vanir/updater/receiver/UpdateCheckReceiver.java
@@ -9,20 +9,28 @@
 
 package com.vanir.updater.receiver;
 
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.ConnectivityManager;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.WindowManager;
 
+import com.vanir.updater.R;
 import com.vanir.updater.misc.Constants;
 import com.vanir.updater.service.UpdateCheckService;
 import com.vanir.updater.utils.Utils;
 
 public class UpdateCheckReceiver extends BroadcastReceiver {
     private static final String TAG = "UpdateCheckReceiver";
+    private static final String GMS_CORE = "com.google.android.gms";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -49,6 +57,24 @@ public class UpdateCheckReceiver extends BroadcastReceiver {
         } else if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
             // We just booted. Store the boot check state
             prefs.edit().putBoolean(Constants.BOOT_CHECK_COMPLETED, false).apply();
+            // Check for the existance of GApps
+            PackageManager pm = context.getPackageManager();
+            try {
+                pm.getPackageInfo(GMS_CORE, PackageManager.GET_ACTIVITIES);
+            } catch (NameNotFoundException e) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle(R.string.app_name);
+                builder.setMessage(R.string.gapps_not_installed);
+                builder.setPositiveButton(R.string.dialog_ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
+                dialog.show();
+            }
         }
         if (Intent.ACTION_CHECK_FOR_UPDATES.equals(action)) {
             Log.i(TAG, "Received quicksettings check request");
